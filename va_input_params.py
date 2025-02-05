3b87b7eb41ee3ae373684524d76e7d369f6d29f3
TiagoBe0 <92551904+TiagoBe0@users.noreply.github.com>
2025-02-05 11:19:00 -0300

    Add files via upload

---
+++ va_input_params.py
@@ -0,0 +1,8 @@
+         
+LAYERS = [{'relax' : 'tests.dump/d',
+          'defect' : 'tests.dump/M_TST_1',
+          'radius' : 2,
+          'smoothing level' : 13, 
+          'cutoff radius' : 3, 
+             'iteraciones':1   #solo si esta activado step_refactor
+          }]
